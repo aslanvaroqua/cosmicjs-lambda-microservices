327fb896be8671736690b195980b5e15c7f36040
Aslan Varoqua <aslan.varoqua@gmail.com>
2018-05-29 12:26:00 -0600

    Update objects.js

--- src/lambda/objects.js
+++ src/lambda/objects.js
@@ -2,12 +2,13 @@ import fetch from "node-fetch";
 
 const API_ENDPOINT =
   "https://api.cosmicjs.com/v1/duas-americas-backend-content/objects?pretty=true&hide_metafields=true";
-//    .then(res => ({
-//      statusCode: 200,
-//      body: `${res.name} `
-//    }))
+
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT)
     .then(response => response.json())
+    .then(res => ({
+         statusCode: 200,
+         body: `${res.json()} `
+     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
